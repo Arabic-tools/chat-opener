399fc7a9917d0cabef441fd3ac5593eadf1f280b
Mohamed Nashaat <mohamed.nashaat@mondia.com>
2021-10-28 14:41:51 +0200

    intail commit

---
+++ components/WhatsAppMsg.vue
@@ -0,0 +1,39 @@
+<template>
+<div>
+
+
+  <div class="card" style="width: 18rem">
+    <img
+      src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
+      class="card-img-top"
+      alt="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
+    />
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">
+        Some quick example text to build on the card title and make up the bulk
+        of the card's content.
+      </p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+  </div>
+  <NuxtLink to="/test">Home page</NuxtLink>
+</div>
+  
+</template>
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+
+  mounted() {
+    this.$nextTick(() => {
+      this.$nuxt.$loading.start();
+      setTimeout(() => this.$nuxt.$loading.finish(), 500);
+    });
+  },
+  name: "WhatsAppMsg",
+  data() {
+    return {};
+  },
+});
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <WhatsAppMsg/>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  head() {
+    return {
+      title: "Home page",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "Home page description",
+        },
+      ],
+      script: [
+        {
+          src: "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js",
+          integrity: "sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p",
+          crossorigin: "anonymous",
+        },
+
+      ],
+      link: [
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900",
+        },
+        {
+          rel: "stylesheet",
+          href: "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css",
+        },
+      ],
+    };
+  },
+});
+</script>
