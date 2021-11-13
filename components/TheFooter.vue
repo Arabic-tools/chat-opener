c87c2908bf80159c593a775bef4ea2457eef5600
Mohamed Nashaat <nashaatfarrag@gmail.com>
2021-11-13 15:19:12 +0200

    fix footer format

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,21 +1,17 @@
 <template>
-  <v-footer dark color="#273443"  padless>
+  <v-footer dark color="#273443" padless>
     <v-row>
-      <v-col cols="12"  class="text-center">
+      <v-col cols="12" class="text-center">
         <!-- الموقع برعاية :
 
           <a href="https://wasetshera.com?myad=53196"> وسيط شراء من الإنترنت</a>
           <br /> -->
-          <p >
-
-        Contact us :
-        <a href="mailto:chatopener@inboxeen.com">chatopener@inboxeen.com</a>
-          </p>
-          <p>
-© 2021 Copyright Chatopener
-          </p>
+        <p>
+          Contact us :
+          <a href="mailto:chatopener@inboxeen.com">chatopener@inboxeen.com</a>
+        </p>
+        <p>© 2021 Copyright Chatopener</p>
       </v-col>
-
     </v-row>
   </v-footer>
 </template>
